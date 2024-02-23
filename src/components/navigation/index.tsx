4cd1ffd1d77e703fc4be651a51f82f5563842e74
Guilherme Galaverna <guigalaverna@gmail.com>
2024-02-23 15:33:12 -0300

    refactor: create collapsible menu

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import { Links } from "./links";
+
+export function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggleNavigator() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <header className="p-12 flex items-center justify-between md:flex-col md:items-start md:h-screen md:w-[15vw]">
+      <button
+        className="bg-transparent text-[0px] outline-none md:hidden"
+        onClick={handleToggleNavigator}
+      >
+        <Menu size={20} className="text-slate-600" />
+      </button>
+      <span className="text-slate-600 font-medium">Guilherme Galaverna</span>
+
+      {isOpen && (
+        <section
+          className="fixed inset-0 w-screen h-screen bg-black/30"
+          onClick={handleToggleNavigator}
+        >
+          <aside className="bg-slate-50 w-[80vw] h-screen p-12 flex flex-col items-start justify-between text-slate-600">
+            <span></span>
+
+            <Links />
+            <button className="text-xl">Close navigation</button>
+          </aside>
+        </section>
+      )}
+
+      <nav className="hidden md:flex text-slate-600">
+        <ul className="text-2xl space-y-5">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About me</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+        </ul>
+      </nav>
+
+      <button className="hidden md:block text-xl text-slate-600">
+        Close navigation
+      </button>
+    </header>
+  );
+}
---
+++ src/components/navigation/links.tsx
@@ -0,0 +1,20 @@
+export function Links() {
+  return (
+    <nav>
+      <ul className="text-2xl space-y-5">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About me</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
